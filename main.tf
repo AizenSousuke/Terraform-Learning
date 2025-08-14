dae6ba00c2df35212ca9acfbd3ad489b95987027
Nik <puteranda_nik@certisgroup.com>
2025-08-14 17:25:18 +0800

    Can create both s3 and ec2

--- main.tf
+++ main.tf
@@ -15,21 +15,21 @@ provider "aws" {
     skip_requesting_account_id  = true
     s3_use_path_style = true
     endpoints {
-        # ec2 = "http://172.17.0.1:4566" # or localhost if running from host
+        ec2 = "http://localhost:4566" # or localhost if running from host
         s3 = "http://localhost:4566" # or localhost if running from host
         # add other services as needed
     }
 }
 
 # Cannot be used as a mock in localstack
-# resource "aws_instance" "web-server" {
-#     ami = "ami-06ca3ca175f37dd66"
-#     instance_type = "t2.micro"
-#     count = 1
-#     tags = {
-#         Name = "web-server-${count.index}"
-#     }
-# }
+resource "aws_instance" "web-server" {
+    ami = "ami-06ca3ca175f37dd66"
+    instance_type = "t2.micro"
+    count = 1
+    tags = {
+        Name = "web-server-${count.index}"
+    }
+}
 
 resource "aws_s3_bucket" "s3-bucket" {
   bucket = "my-test-bucket"
